2d1e75c6d74512800d1a6b23447f4ad20b5df1c7
Li-Yaru <liliyaru@126.com>
2020-04-21 11:57:32 +0800

    2020-4-21

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180221/Ch10Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180221/Ch10Activity1.java
@@ -3,25 +3,38 @@ package cn.edu.sdwu.android02.classroom.sn170507180221;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class Ch10Activity1 extends AppCompatActivity {
+    private Integer count;//点击按键的计数器
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_ch10_1);
+        count=0;
 
-        Log.i(Ch10Activity1.class.toString(),"onCreate");
     }
-
+    //计数的方法
+    public void counter(View view){
+        count++;
+        Log.i(Ch10Activity1.class.toString(),"counter"+count);
+    }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        //使用本方法保存一些界面的状态信息
+        //数据保存到Bundle对象中
+        outState.putInt("counter",count);
         super.onSaveInstanceState(outState);
+        Log.i(Ch10Activity1.class.toString(),"onSaveInstanceState");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        //恢复之前保存的状态信息
+        count=savedInstanceState.getInt("counter");
+        Log.i(Ch10Activity1.class.toString(),"onRestoreInstanceState");
     }
 
     @Override
