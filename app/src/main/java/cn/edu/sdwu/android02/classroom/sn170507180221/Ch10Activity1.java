027623c1dd7d4df670b5c022e6d8704b41e8f737
Li-Yaru <liliyaru@126.com>
2020-04-21 10:16:19 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180221/Ch10Activity1.java
@@ -0,0 +1,63 @@
+package cn.edu.sdwu.android02.classroom.sn170507180221;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Ch10Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch10_1);
+
+        Log.i(Ch10Activity1.class.toString(),"onCreate");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Ch10Activity1.class.toString(),"onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Ch10Activity1.class.toString(),"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Ch10Activity1.class.toString(),"onDestroy");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Ch10Activity1.class.toString(),"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Ch10Activity1.class.toString(),"onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Ch10Activity1.class.toString(),"onRestart");
+    }
+
+}
